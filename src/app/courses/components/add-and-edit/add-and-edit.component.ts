49390dfab9abc0ecb692bf772cfdd69377da5371
AshkanIzadpanah <ashkan@Ashkans-MacBook-Pro.local>
2020-12-31 15:42:46 -0500

    stylying and fix edit mode

--- src/app/courses/components/add-and-edit/add-and-edit.component.ts
+++ src/app/courses/components/add-and-edit/add-and-edit.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
 import { Course } from '../../models/course';
 import { CourseService } from '../../services/course.service';
-import { Router } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +12,8 @@ import { Router } from '@angular/router';
 })
 export class AddAndEditComponent implements OnInit {
   //reactive forms - dynamically creating formGroup and adding formControls to it
-
+  editMode:boolean = false;
+  courseId:number= -1;
   courseForm =  this.formBuilder.group({
     title: ['',  Validators.required],
     topic: ['',  Validators.required],
@@ -22,16 +22,32 @@ export class AddAndEditComponent implements OnInit {
 
   constructor(private formBuilder:FormBuilder, 
     private router:Router,
+    private activatedRoute:ActivatedRoute,
     private courseservice:CourseService) { }
 
   ngOnInit(): void {
- 
+    this.activatedRoute.paramMap.subscribe(param => {
+      this.editMode = param.get('id') !== null;
+      //console.log(' edit mode' + id);
+      if(this.editMode){
+        this.courseId =  Number(param.get("id"));
+        this.courseservice.getCourseById(this.courseId).subscribe(item=> {
+          //console.log('item -->' + item);
+          this.courseForm.get('title')?.setValue(item?.title);
+          this.courseForm.get('topic')?.setValue(item?.topic);
+          this.courseForm.get('description')?.setValue(item?.description);
+        });
+      }
+    });
   }
 
   save():void{
     let course = new Course();
     console.log('course' + this.courseForm.get("title")?.value);
-
+    if(this.editMode){
+      course.id = this.courseId;
+      this.courseId = -1;
+    }
     course.title = this.courseForm.get("title")?.value;
     course.topic = this.courseForm.get("topic")?.value;
     course.description = this.courseForm.get("description")?.value;
--- src/app/courses/services/course.service.ts
+++ src/app/courses/services/course.service.ts
@@ -7,18 +7,24 @@ import { Course, ICourse } from '../models/course';
 })
 export class CourseService {
   courses:Course[] = [
-    {id:0, title:'	Google Certified Associate Cloud Engineer Certification - Udemy', topic:'GCP', description:'Get hands on with Google Cloud Platform (GCP) and become a Google Certified Associate Cloud Engineer (ACE)'},
-    {id:1, title:'	Grokking the System Design Interview', topic:'Distributed Systems', description:'This course is a complete guide to master the system design interviews. It is created by hiring managers who’ve been working at Google, Facebook, Microsoft, and Amazon.'},
-    {id:2, title:'Spring Boot', topic:'Spring', description:'Spring Core, Spring MVC, Spring initializr, embedded server, '},
-    {id:3, title:'Spring Data', topic:'Spring', description:'Spring Data jpa, Spring Data mongodb, hibernate, CrudRepository'},
-    {id:4, title:'Angular 7 Crash Course', topic:'Angular', description:'Typescript, Webpack, Angular-cli, Dependency Injections, Components, Services, Routers, Reactive Forms,'},
-    {id:5, title:'Java Core', topic:'Java', description:'Collections, Streams, ExecutorService'},
-    {id:6, title:'JDK 8 Lambdas', topic:'Java', description:'Functions, BiFunctions, Operators, Producers, Consumers'},
-    {id:7, title:'JDK 9 Flows', topic:'Java', description:'RxJava, Flow.Publisher, Flow.Subscriber'},
-
+    {id:1, title:'	Google Certified Associate Cloud Engineer Certification - Udemy', topic:'GCP', description:'Get hands on with Google Cloud Platform (GCP) and become a Google Certified Associate Cloud Engineer (ACE)'},
+    {id:2, title:'	Grokking the System Design Interview', topic:'Distributed Systems', description:'This course is a complete guide to master the system design interviews. It is created by hiring managers who’ve been working at Google, Facebook, Microsoft, and Amazon.'},
+    {id:3, title:'Spring Boot', topic:'Spring', description:'Spring Core, Spring MVC, Spring initializr, embedded server, '},
+    {id:4, title:'Spring Data', topic:'Spring', description:'Spring Data jpa, Spring Data mongodb, hibernate, CrudRepository'},
+    {id:5, title:'Angular 7 Crash Course', topic:'Angular', description:'Typescript, Webpack, Angular-cli, Dependency Injections, Components, Services, Routers, Reactive Forms,'},
+    {id:6, title:'Java Core', topic:'Java', description:'Collections, Streams, ExecutorService'},
+    {id:7, title:'JDK 8 Lambdas', topic:'Java', description:'Functions, BiFunctions, Operators, Producers, Consumers'},
+    {id:8, title:'JDK 9 Flows', topic:'Java', description:'RxJava, Flow.Publisher, Flow.Subscriber'},
   ];
+  generatedId:number=this.courses.length;
+
 
   constructor() { }
+
+  nextId():number{
+    this.generatedId = this.generatedId + 1;
+    return this.generatedId;
+  }
 
   getAllCourses():Observable<ICourse[]> {
     return of(this.courses);
@@ -35,21 +41,24 @@ export class CourseService {
   addOrUpdateCourse(course:ICourse):void{
     this.courses.sort();
     console.log('addOrUpdateCourse' + course);
-    if(!course.id){//inset
-      course.id = this.courses.length + 1;
+    if(course.id === -1 || course.id === undefined){//insert
+      course.id = this.nextId();
       this.courses.push(course);
       return;
     }
     //update
-    let existingCourse = this.courses.find(item => item.id = course.id);
-    existingCourse = course;
+    let existingCourse = this.courses.find(item => item.id === course.id);
+    if(existingCourse !== undefined){
+      existingCourse.title = course.title;
+      existingCourse.topic = course.topic;
+      existingCourse.description = course.description;
+    }
   }
 
   deleteCourse(course:ICourse):void{
-    let index = this.courses.findIndex(item => item.id=course.id);
+    console.log('deleteCourse ');
+    let index = this.courses.findIndex(item => item.id === course.id);
     this.courses.splice(index, 1);
     console.log('deleteCourse ' + course);
   }
-
-
 }
