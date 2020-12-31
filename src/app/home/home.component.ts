21808774a40888e1cd663b2071f41d5d66f16b35
AshkanIzadpanah <ashkan@Ashkans-MacBook-Pro.local>
2020-12-31 04:06:54 -0500

    home page changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  COPY_RIGHT:string = 'https://github.com/AshkIza/Angular';
 
   constructor() { }
 
